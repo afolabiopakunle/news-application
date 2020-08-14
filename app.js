383f46b16a278e35c297edd7d259b29d78460dbb
afolabiopakunle <opakunleafolabi@gmail.com>
2020-08-14 17:48:24 +0100

    mongoose connection established and tested with first schema

--- app.js
+++ app.js
@@ -16,8 +16,27 @@ mongoose.connect("mongodb://localhost/news-application", {
     useUnifiedTopology: true
 });
 
+//NEWS SCHEMA
+const newSchema = mongoose.Schema({
+    title: String,
+    picture: String,
+    body: String,
+    createdOn: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const News = mongoose.model("New", newSchema);
+
+// News.create({
+//     title: "AlphaBlog is LIVE!!!",
+//     picture: "https://naijanetworth.com/wp-content/uploads/2019/04/Newspapers.jpg",
+//     body: "A Nigerian newspaper and Online version of the Vanguard, a daily publication in Nigeria covering Nigeria news, Niger delta, general national news, politics, ..."
+// })
+
 app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.listen(3000, () => console.log(`app running on port ${PORT} http://localhost:${PORT}`))
+app.listen(3000, () => console.log(`app running on port ${PORT} http://localhost:${PORT}`));
