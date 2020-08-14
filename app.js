4375053459c7c383d1ea160fe1e407bfa371f021
afolabiopakunle <opakunleafolabi@gmail.com>
2020-08-14 17:14:11 +0100

    index page added app running

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://localhost/news-application", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.listen(3000, () => console.log(`app running on port ${PORT} http://localhost:${PORT}`))
